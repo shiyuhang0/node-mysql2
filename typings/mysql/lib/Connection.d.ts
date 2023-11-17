ded530ef446ae6ce0d96bc435efebe357ec6486f
shiyuhang <1136742008@qq.com>
2023-11-17 10:42:53 +0800

    copy-from-2099

--- typings/mysql/lib/Connection.d.ts
+++ typings/mysql/lib/Connection.d.ts
@@ -297,6 +297,8 @@ export interface ConnectionOptions {
   authPlugins?: {
     [key: string]: AuthPlugin;
   };
+
+  useStaticParser?: boolean;
 }
 
 declare class Connection extends QueryableBase(ExecutableBase(EventEmitter)) {
